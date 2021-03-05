31918080e4065ba1fffb57fb6b9cb3719db5fd35
Sander Hautvast <shautvast@gmail.com>
2021-03-05 14:33:48 +0100

    mmore simplify

--- src/js/index.js
+++ src/js/index.js
@@ -274,7 +274,7 @@ const function_call = function (function_name, argument_exprs) {
     if (Object.prototype.hasOwnProperty.apply(functions, [function_name])) {
         return functions[function_name](resolve_arguments(argument_exprs));
     } else {
-        return 'unimplemented: ' + function_name + '(' + argument_exprs.map(e=>e.value_type).join(',') + ')';
+        return `unimplemented:  ${function_name}(${argument_exprs.map(e=>e.value_type).join(',') })`;
     }
 }
 
