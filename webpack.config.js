948c0bc50f3069d8f3ad82bdbd39792789033268
Sander Hautvast <shautvast@gmail.com>
2021-03-05 11:38:47 +0100

    added webpack html plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     mode: 'development',
@@ -7,6 +8,7 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
+        clean: true,
     },
     module: {
         rules: [
@@ -39,4 +41,10 @@ module.exports = {
             },
         ],
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            hash: true,
+            filename: 'index.html'
+        })
+    ]
 };
